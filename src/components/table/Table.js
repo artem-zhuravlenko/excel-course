f09b8f778e4e93620ec1b14fc48ebca16aafe7e2
artif1c1al <zhuravlenkoartem@gmail.com>
2020-08-19 13:13:24 +0300

    finish resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,8 @@
 import { ExcelComponent } from "@core/ExcelComponent";
 import { createTable } from "./table.template";
 import { $ } from '../../core/dom'
+import { resizeHandler } from "./table.resize";
+import { shouldResize } from "./table.functions";
 
 export class Table extends ExcelComponent {
   constructor($root) {
@@ -15,34 +17,8 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $target = $(event.target)
-      const $parent = $target.closest('[data-type="resizable"]')
-      const coords = $parent.getCoords()
-      const cols = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-      const type = $target.data.resize
-
-      const colRes = e => {
-        const delta = e.pageX - coords.right
-        const value = coords.width + delta
-        // $parent.$el.style.width = value + 'px'
-        $parent.css({ width: value + "px" })
-        cols.forEach(el => el.style.width = value + 'px')
-      }
-
-      const rowRes = e => {
-        const delta = e.pageY - coords.bottom
-        const value = coords.height + delta
-        $parent.css({ height: value + 'px' })
-      }
-
-      document.onmousemove = e => {
-        type === 'col' ? colRes(e) : rowRes(e)
-      }
-    }
-
-    document.onmouseup = () => {
-      document.onmousemove = null
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
     }
   }
 }
